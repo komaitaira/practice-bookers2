00e4a0383add46bba7daefd2c4afcb0ceaae1d12
komaitaira <komaitaira@gmail.com>
2020-06-24 20:34:07 +0900

    finish debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :baria_user, only: [:update]
+  before_action :baria_user, only: [:update, :edit]
 
   def show
     @user = User.find(params[:id])
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = current_user
     @users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
     @book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
@@ -20,9 +21,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "successfully updated user!"
+      redirect_to user_path(@user.id), notice: "successfully updated user!"
     else
-      render "show"
+      render "edit"
     end
   end
 
@@ -32,10 +33,10 @@ class UsersController < ApplicationController
   end
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
-   def baria_user
+  def baria_user
     unless params[:id].to_i == current_user.id
       redirect_to user_path(current_user)
     end
-   end
+  end
 
 end
